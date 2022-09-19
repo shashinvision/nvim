644c9980856fe05b493aa5eff8c9fe4bb8f00cce
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-09-18 23:39:29 -0300

    update tab and line 2

--- lua/plugins/config/bufferline.lua
+++ lua/plugins/config/bufferline.lua
@@ -1,3 +1,15 @@
-require('bufferline').setup {
+    require('bufferline').setup {
+        options= {
+            mode = "buffers",
+            numbers = "ordinal",
+            close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
+            right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+            left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
+            middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
+            separator_style = "thin",
+            diagnostics = "coc"
+        },
 
-}
+        highlights = {
+        }
+    }
