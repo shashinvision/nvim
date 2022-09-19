35a92da0b400f78cfade1f20a3fde04b11b38e64
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-09-18 23:44:07 -0300

    update tab and line 2

--- lua/plugins/config/bufferline.lua
+++ lua/plugins/config/bufferline.lua
@@ -1,15 +1,17 @@
-    require('bufferline').setup {
-        options= {
-            mode = "buffers",
-            numbers = "ordinal",
-            close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
-            right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-            left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-            middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-            separator_style = "thin",
-            diagnostics = "coc"
-        },
-
-        highlights = {
-        }
+require('bufferline').setup {
+  options = {
+    mode = 'buffers',
+    offsets = {
+      {filetype = 'NvimTree'}
+    },
+  },
+  highlights = {
+    buffer_selected = {
+      italic = false
+    },
+    indicator_selected = {
+      fg = {attribute = 'fg', highlight = 'Function'},
+      italic = false
     }
+ }
+}
