95c32781c5ba51d9a64fc54d7ac0f597245809c1
Felipe Mancilla <felipei.mancilla@gmail.com>
2023-04-18 10:33:32 -0400

    ADD - COonfig on nvimtree

--- lua/config/plugins/nvimtree.lua
+++ lua/config/plugins/nvimtree.lua
@@ -1,22 +1,28 @@
--- examples for your init.lua
-
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
+-- Show dotfiles by default
+vim.g.nvim_tree_hide_dotfiles = 0
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
+require'nvim-tree'.setup({
   sort_by = "case_sensitive",
   renderer = {
     group_empty = true,
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md", ".env", "env", "TODO" },
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
+    git_clean = false,
+    exclude = {},
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    timeout = 400,
   },
 })
